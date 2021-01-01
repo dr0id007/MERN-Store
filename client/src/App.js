65a1f598cee620a888f9d00f3bdacc626acf9561
Dev Arora <devarora745@gmail.com>
2021-01-01 10:07:37 +0530

    lazy loading aded

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,36 @@
-import React from 'react'
-import { BrowserRouter , Route , Switch } from 'react-router-dom'
-import { connect } from 'react-redux'
-// import  { mapStateToProps , mapDispatchToProp } from './reducers/reducer'
-import Home from './views/Home/index'
-import Navbar from './components/Navbar/navbar'
-import Register from './views/Login/Register'
-import Login from './views/Login/Login'
-import Product from './views/Product/index'
-import Order from './views/Order/Order'
-import PrivateRoute from  './hoc/PrivateRoute'
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar/navbar';
+import PrivateRoute from './hoc/PrivateRoute';
+import Loader from './components/Loader/loader';
 
-import './bootstrap.min.css' 
-import './App.css'
+import './bootstrap.min.css';
+import './App.css';
+
+const Home = lazy(() => import('./views/Home'));
+const Register = lazy(() => import('./views/Login/Register'));
+const Login = lazy(() => import('./views/Login/Login'));
+const Product = lazy(() => import('./views/Product'));
+const Order = lazy(() => import('./views/Order/Order'));
 
 const App = (props) => {
-
-  return (
-    <BrowserRouter>
-    <>
-    <Navbar />
-      <Switch>
-        <Route exact path={'/register'} component={Register} />
-        <Route exact path={'/login'} component={Login} />
-        <PrivateRoute path={'/product/:id'} component={Product} />
-        <PrivateRoute exact path={'/order'} component={Order} />
-        <PrivateRoute path={'/'} component={Home} />
-      </Switch>
-      </>
-    </BrowserRouter>
-  )
-}
+	return (
+		<BrowserRouter>
+			<>
+				<Navbar />
+				<Suspense fallback={<Loader />}>
+					<Switch>
+						<Route exact path={'/register'} component={Register} />
+						<Route exact path={'/login'} component={Login} />
+						<PrivateRoute path={'/product/:id'} component={Product} />
+						<PrivateRoute exact path={'/order'} component={Order} />
+						<PrivateRoute path={'/'} component={Home} />
+					</Switch>
+				</Suspense>
+			</>
+		</BrowserRouter>
+	);
+};
 
 export default App;
 // export default connect(mapStateToProps , mapDispatchToProp , null )(App)
--- client/src/hoc/PrivateRoute.js
+++ client/src/hoc/PrivateRoute.js
@@ -1,32 +1,37 @@
-import React , {useState , useEffect} from 'react'
-import { useSelector , useDispatch } from 'react-redux'
-import {Route} from 'react-router-dom'
-import {isAuth} from '../api/auth'
-import Loader from '../components/Loader/loader'
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Route } from 'react-router-dom';
+import { isAuth } from '../api/auth';
+import Loader from '../components/Loader/loader';
 
-const PrivateRoute = ({component:Component , ...props}) => {
+const PrivateRoute = ({ component: Component, ...props }) => {
+	const [loading, setLoading] = useState(false);
+	const [isLogged, setIsLogged] = useState(false);
+	const state = useSelector((state) => state);
+	const dispatch = useDispatch();
 
-    const [loading , setLoading] = useState(false)
-    const [isLogged , setIsLogged] = useState(false)
-    const state = useSelector(state => state)
-    const dispatch = useDispatch()
+	const init = async () => {
+		const res = await isAuth();
+		setIsLogged(res.auth);
+		dispatch({ type: 'SET_AUTH', payload: { user: res.user, auth: res.auth } });
+	};
 
-    const init = async () =>  {
-       const res =  await isAuth()
-       setIsLogged(res.auth)
-       dispatch({ type: 'SET_AUTH' , payload: {user: res.user , auth: res.auth} })
-    }
-    
-    useEffect(() => {
-        init()
-    } , [])
+	useEffect(() => {
+		init();
+	}, []);
 
-    if(loading) {
-        return <Loader />
-    }
+	if (loading) {
+		return <Loader />;
+	}
 
-    return <Route {...props} render={(props) => 
-    { return <Component {...props} auth={isLogged} /> }} /> 
-}
+	return (
+		<Route
+			{...props}
+			render={(props) => {
+				return <Component {...props} auth={isLogged} />;
+			}}
+		/>
+	);
+};
 
-export default PrivateRoute
+export default PrivateRoute;
