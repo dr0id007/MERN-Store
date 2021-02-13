9000dffd887f10a9b77c6492a9fe0ce4dd82ba91
Dev Arora <devarora745@gmail.com>
2021-02-13 19:56:03 +0530

    build added

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const cors = require("cors");
-const config = require("config");
-const bodyParser = require("body-parser");
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const config = require('config');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
 // Connect Database
 connectDB();
@@ -17,10 +18,15 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // Routes
-app.use('/api/auth' , require('./routes/auth'))
+app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
-app.use('/api/product' , require('./routes/products'))
-app.use('/api/payment' , require('./routes/payment'))
+app.use('/api/product', require('./routes/products'));
+app.use('/api/payment', require('./routes/payment'));
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
