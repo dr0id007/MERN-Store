3cf354263884dc51bd057ed83df3665dea87f90f
dr0id007 <devarora745@gmail.com>
2021-04-19 23:39:24 +0530

    env changes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,25 +3,25 @@ const config = require('config')
 
 const authenticateToken = (req, res, next) => {
 	// Get token from header
-	const token = req.header('auth-token');
+	const token = req.header('auth-token')
 
 	// Check if token available
 	if (!token) {
-		return res.status(401).json({ msg: 'No token, authorization denied' });
+		return res.status(401).json({ msg: 'No token, authorization denied' })
 	}
 
 	// Verify token
 	try {
-		const decoded = jwt.verify(token, config.get('jwtSecret'));
-		req.user = decoded.user;
-		next();
+		const decoded = jwt.verify(token, process.env.JWT_SECRET)
+		req.user = decoded.user
+		next()
 	} catch (err) {
-		res.status(401).json({ msg: 'Token is not valid' });
+		res.status(401).json({ msg: 'Token is not valid' })
 	}
-};
-
-const generateToken = (payload) => {
-    return jwt.sign(payload , config.get('jwtSecret') , {expiresIn: '60s'})
 }
 
-module.exports = {authenticateToken , generateToken};
+const generateToken = (payload) => {
+	return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '60s' })
+}
+
+module.exports = { authenticateToken, generateToken }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,32 +1,26 @@
-const express = require('express');
-const router = express.Router();
-const bcrypt = require('bcryptjs');
-const {authenticateToken:auth} = require('../middleware/auth');
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const { check, validationResult } = require('express-validator');
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcryptjs')
+const { authenticateToken: auth } = require('../middleware/auth')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const { check, validationResult } = require('express-validator')
 
-const User = require('../models/User');
+const User = require('../models/User')
 
-// @route    GET api/auth
-// @desc     Get logged user
-// @access   Private
 router.get('/', auth, async (req, res) => {
 	try {
-		const user = await User.findById(req.user.id).select('-password');
-		if(!user) {
+		const user = await User.findById(req.user.id).select('-password')
+		if (!user) {
 			res.send('User not found')
 		}
-		res.json(user);
+		res.json(user)
 	} catch (err) {
-		console.error('error' ,err.message);
-		res.status(500).send('Server Error');
+		console.error('error', err.message)
+		res.status(500).send('Server Error')
 	}
-});
+})
 
-// @route    POST api/auth
-// @desc     Authenticate user & get token
-// @access   Public
 router.post(
 	'/login',
 	[
@@ -34,44 +28,38 @@ router.post(
 		check('password', 'Password is required').exists()
 	],
 	async (req, res) => {
-		const errors = validationResult(req);
+		const errors = validationResult(req)
 		if (!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() });
+			return res.status(400).json({ errors: errors.array() })
 		}
-		const { email, password } = req.body;
+		const { email, password } = req.body
 		try {
-			let user = await User.findOne({ email });
+			let user = await User.findOne({ email })
 			if (!user) {
-				return res.status(400).json({ msg: 'Invalid Credentials' });
+				return res.status(400).json({ msg: 'Invalid Credentials' })
 			}
-			const isMatch = await bcrypt.compare(password, user.password);
+			const isMatch = await bcrypt.compare(password, user.password)
 			if (!isMatch) {
-				return res.status(400).json({ msg: 'Invalid Credentials' });
+				return res.status(400).json({ msg: 'Invalid Credentials' })
 			}
 			// if (!user.isVerified) {
-			// 	return res.status(401).json({ type: 'not-verified', msg: 'Your account has not been verified.' }); 
+			// 	return res.status(401).json({ type: 'not-verified', msg: 'Your account has not been verified.' });
 			// }
 			const payload = {
 				user: {
 					id: user.id
 				}
-			};
+			}
 
-			jwt.sign(
-				payload,
-				config.get('jwtSecret'),
-				{ expiresIn: 360000 },
-				(err, token) => {
-					if (err) throw err;
-					res.json({ token });
-				}
-			);
+			jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 360000 }, (err, token) => {
+				if (err) throw err
+				res.json({ token })
+			})
 		} catch (err) {
-			console.error(err.message);
-			res.status(500).send('Server error');
+			console.error(err.message)
+			res.status(500).send('Server error')
 		}
 	}
-);
+)
 
-
-module.exports = router;
+module.exports = router
